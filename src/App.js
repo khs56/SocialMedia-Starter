a311cc69b414a96e72d40af9793933b84486823f
Vishal M <48333506+khs56@users.noreply.github.com>
2024-05-21 09:52:43 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css"
+import Home from "./pages/Home/Home";
 function App() {
   return (
     <div className="App">
-        Subscribe Zainkeeps code
-    </div>
+      <div className="blur" style={{ top: '-18%', right: '0' }}></div>
+      <div className="blur" style={{
+        top: '36%', left: '-8rem'
+      }}></div >
+      <Home />
+    </div >
   );
 }
 
