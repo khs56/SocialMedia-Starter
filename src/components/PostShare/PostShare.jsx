f414c1edcca8675ac865ac9a03d3654a4ec1b7e0
Vishal M <48333506+khs56@users.noreply.github.com>
2024-05-21 12:00:40 -0400

    Create PostShare.jsx

---
+++ src/components/PostShare/PostShare.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './PostShare.css'
+import ProfileImage from '../../img/profileImg.jpg'
+
+const PostShare = () => {
+  return (
+    <div className="PostShare">
+      <img src={ProfileImage} alt="" />
+    </div>
+  )
+}
+
+export default PostShare
