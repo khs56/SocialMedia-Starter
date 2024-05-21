21190779c9f06cfc6c10379dcb396da5a3b9344a
Vishal M <48333506+khs56@users.noreply.github.com>
2024-05-21 12:01:01 -0400

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { PostSide } from '../../components/PostSide/PostSide'
 import ProfileSide from '../../components/Profile/ProfileSide'
 import './Home.css'
 
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div className="Home">
       <ProfileSide />
+      <PostSide />
       <div className="profileSide">Profile</div>
       <div className="postSide">Posts</div>
       <div className="RightSide">Right Side</div>
