11c54b2bfed16cf7c061411822fbf811082afaec
Vishal M <48333506+khs56@users.noreply.github.com>
2024-05-21 15:04:36 -0400

    Create Posts.jsx

---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Posts.css'
+import { PostsData } from '../../Data/PostsData'
+import Post from '../Post/Post'
+
+const Posts = () => {
+  return (
+    <div className="Posts">
+      {PostsData.map((post, id) => { return <Post /> })}
+    </div>
+  )
+}
+
+export default Posts
